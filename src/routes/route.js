c94f02757f300f6c0f75ef7938f29ba0e626e69a
RameshwarNavathar <rameshwarbhagavan99@gmail.com>
2022-10-20 20:43:17 +0530

    update the post apis

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,14 @@
 const express = require('express');
+// const { last } = require('underscore');
+// const { result } = require('underscore');
 const router = express.Router();
 
 router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+    let studentName=[{'ram':22,'sham':33,'jay':99}]
+    let  a= studentName.req.params.name
+    
+    console.log(a)
+    res.send(a)
 })
 
 router.get("/random" , function(req, res) {
@@ -69,5 +73,95 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+router.post('/Ramaa',function(req,res){
+    let arr=[22,"Navathar"]
+    let ele=req.body.element
+    let ele1=req.body.user
+    arr.push(ele,ele1)
+
+    res.send({arr})
+});
+//:::::::::::::::::::::::::::::::::::::::::::::::1:::::::::::::::::::::::::::::::::::::::::::::::::::
+router.get("/sol1", function (req, res) {
+    
+    let arr= [1,2,3,4,6,7]
+    let n=arr.length+1
+    let missingNumber=n*(n+1)/2//7*8/2=28
+    sum=0
+    for(i=0;i<arr.length;i++){
+    sum+=arr[i]
+    }
+   let result=missingNumber-sum
+   
+
+    res.send( { data:result} );
+    });
+
+
+//:::::::::::::::::::::::::::::::::::::::::::::::2::::::::::::::::::::::::::::::::::::::::::::::;
+    router.get("/sol12", function (req, res) {
+    
+        let arr= [33, 34, 36, 37, 38]
+
+        let n=arr.length+1
+        let missingNumber=n*(33+38)/2
+        sum=0
+        for(i=0;i<arr.length;i++){
+        sum+=arr[i]
+        }
+       let result=missingNumber-sum
+       
+        res.send( { data:result} );
+        });
+
+//::::::::::::::::::::3:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [ 
+               "soccer"
+           ]
+       },
+   ]
+   router.post("/digit", function (req, res) {
+    
+const body =req.body
+const ram=players.find(x=>x.name===body.name)
+
+if (ram){
+    res.send({message:"pleyers allredy exist"})
+
+}else{ 
+    players.push(body)
+    res.send(players)
+}})
+
+
+
+ 
 
 module.exports = router;
