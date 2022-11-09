1cf929c6f40ccd0e4bc4e3040b881f4a31d04857
RameshwarNavathar <rameshwarbhagavan99@gmail.com>
2022-11-09 07:44:27 +0530

    try-catch simpale assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,31 @@
 const UserModel= require("../models/userModel")
 
 
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+const createUser= async function (req, res) {
+    try{
+    let data= req.body
+    let savedData= await UserModel.create(data)
+    res.status(201).send({msg: savedData})
+    }
+    catch (err){  
+res.status(400).send({msg:err.message})
+    }
+};
+ 
+const loggin= async function (req, res) {
+    // try{
+        let id=req.params.userId
+        let pass=await UserModel.findById(id)
+        if(!pass){
+            res.status(200).send({msg:pass})
+         }
+        let email=req.body.emailId
+    let allUsers= await UserModel.findOne({emailId:email},{})
+    res.status(200).send({msg: allUsers})
+    // }catch (err){
+    //     res.status(404).send({msg:err.message})
     }
 
 
+module.exports={createUser,loggin}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
--- src/index.js
+++ src/index.js
@@ -8,18 +8,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Rameshwar:f8DhVaFG52IaRaDl@rameshwar-n.wdhggi2.mongodb.net/Rameshwar?retryWrites=true&w=majority", {
     useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
+})  
+.then( () => console.log("Database is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
 
 app.use('/', route);
 
